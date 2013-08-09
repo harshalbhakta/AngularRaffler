af54a78d94a43690c25f515e80eb455db20c238e
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2013-08-09 23:29:19 +0530

    Generated rails app & added angular.js file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngularRaffler::Application.config.secret_token = 'ec5c5b4a05f6858c29ee9eac719549f3ec3d9b098ed2fedba25ab4dc45b13a12fcf263df073ae38c97eb062bb91c651f35525eb60a71152b473056e7bfb32328'
